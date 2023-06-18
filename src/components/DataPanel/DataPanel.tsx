5470f84a1f01d40dcd9486c3c96fd83c745c92eb
Joaquin Enriques <joaquin.enriques@gmail.com>
2023-06-18 08:55:58 -0300

    Update DataPanel.tsx

--- src/components/DataPanel/DataPanel.tsx
+++ src/components/DataPanel/DataPanel.tsx
@@ -54,7 +54,8 @@ function DataPanel(props: Props) {
       .map((isHidden: boolean, idx: number) => {
         if (isHidden) return true;
         return !Object.values(ramenStores[idx])
-          .join().toLowerCase()
+          .join()
+          .toLowerCase()
           .includes(controlOption.search.toLowerCase());
       });
   }, [props.controlOption, checkList]);
